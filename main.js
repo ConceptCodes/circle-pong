7edc84d04e08d677708f273979f23f068e73c7d4
Concept <conceptcodes@gmail.com>
2021-06-20 10:31:28 -0400

    added paddle and ball

--- main.js
+++ main.js
@@ -1,17 +1,65 @@
-let width;
+let paddle, ring, ball;
 
+//----------- Ball -------------------------//
+class Ball {
+    constructor() {
+
+    }
+}
+//----------- Paddle -------------------------//
+class Paddle {
+    constructor() {
+        this.x = 0
+        this.y = 0
+        this.scalar = 200
+        this.angle = 0
+    }
+
+    render() {
+        fill('green')
+        rect(this.x, this.scalar-10, 55, 20, 20);
+    }
+
+    moveRight() {
+        this.x = this.x + this.scalar * cos(this.angle);
+        this.y = this.y + this.scalar * sin(this.angle);
+        this.angle++
+    }
+
+    moveLeft() {
+        this.x = this.x - this.scalar * cos(this.angle);
+        this.y = this.y - this.scalar * sin(this.angle);
+        this.angle++
+    }
+}
+//----------- Outer Ring -------------------------//
+class Ring {
+    constructor() {
+        this.width = 400
+    }
+    render() {
+        strokeWeight(5)
+        stroke('white')
+        fill(13,17,21)
+        ellipse(0,0,this.width)
+    }
+}
+//----------- Main P5 -------------------------//
 function setup(){
-    width = 400;
-    createCanvas(500, 500);
+    createCanvas(500, 500)
+    angleMode(DEGREES)
+    paddle = new Paddle()
+    ring = new Ring()
 }
 
 function draw() {
     background(13, 17, 21)
-
     translate(window.width/2, window.height/2)
-
-    strokeWeight(5)
-    stroke('white')
-    fill(13,17,21)
-    ellipse(0,0,width)
+    ring.render()
+    paddle.render()
 }
+
+function keyPressed() {
+    if (keyCode === LEFT_ARROW) { paddle.moveLeft() } 
+    else if (keyCode === RIGHT_ARROW) { paddle.moveRight() }
+  }
